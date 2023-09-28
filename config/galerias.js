9921c4b92c8d0456400426a36d90005bfd9a3a91
ferbal <ferbal2000@gmail.com>
2023-09-28 04:03:09 -0300

    Script fotos galeria

---
+++ config/galerias.js
@@ -0,0 +1,49 @@
+// Galerías
+let galerias = {
+    jornada1: {
+        1:[
+            'media/img/jornada-1/1/foto-02.jpeg',
+            'media/img/jornada-1/1/foto-03.jpeg',
+            'media/img/jornada-1/1/foto-04.jpeg',
+            'media/img/jornada-1/1/foto-05.jpeg',
+            'media/img/jornada-1/1/foto-06.jpeg',
+            'media/img/jornada-1/1/foto-07.jpeg',
+            'media/img/jornada-1/1/foto-08.jpeg',
+            'media/img/jornada-1/1/foto-09.jpeg',
+            'media/img/jornada-1/1/foto-10.jpeg',
+            'media/img/jornada-1/1/foto-11.jpeg',
+            'media/img/jornada-1/1/foto-12.jpeg',
+            'media/img/jornada-1/1/foto-13.jpeg',
+            'media/img/jornada-1/1/foto-14.jpeg',
+            'media/img/jornada-1/1/foto-15.jpeg',
+            'media/img/jornada-1/1/foto-16.jpeg',
+        ],
+        2: [
+            'media/img/jornada-1/2/foto-17.jpeg',
+            'media/img/jornada-1/2/foto-18.jpeg',
+            'media/img/jornada-1/2/foto-19.jpeg',
+            'media/img/jornada-1/2/foto-20.jpeg',
+            'media/img/jornada-1/2/foto-21.jpeg',
+            'media/img/jornada-1/2/foto-22.jpeg',
+            'media/img/jornada-1/2/foto-23.jpeg',
+            'media/img/jornada-1/2/foto-24.jpeg',
+            'media/img/jornada-1/2/foto-25.jpeg',
+            'media/img/jornada-1/2/foto-26.jpeg',
+        ],
+        resumen: [
+            'media/img/jornada-1/resumen/foto-01.jpeg',
+            'media/img/jornada-1/resumen/foto-02.jpeg',
+            'media/img/jornada-1/resumen/foto-03.jpeg',
+            'media/img/jornada-1/resumen/foto-04.jpeg',
+        ],
+    },
+    jornada2: {
+
+    },
+    jornada3: {
+
+    },
+    jornada4: {
+
+    },
+};
